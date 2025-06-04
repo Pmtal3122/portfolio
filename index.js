307000e156ba215cfac7be6bc876ba80c2968d6a
Pmtal3122 <talegaonkar.piyush@gmail.com>
2025-06-04 10:38:04 +0530

    Added lenis and shifted three js code to a index js

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-gsap.registerPlugin(ScrollTrigger,ScrollToPlugin);
+import * as THREE from 'three';
+import { GLTFLoader } from "three/addons/loaders/GLTFLoader.js";
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js';
+
+// Initialize Lenis
+const lenis = new Lenis({
+    autoRaf: true,
+});
+
+const renderer = new THREE.WebGLRenderer({
+    antialias: true
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(window.devicePixelRatio);
+renderer.setClearColor(0xffffff, 1.0);
+document.querySelector('.container').appendChild(renderer.domElement);
+
+const loader = new GLTFLoader();
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath('https://www.gstatic.com/draco/v1/decoders/');
+loader.setDRACOLoader(dracoLoader);
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+const loadModel = async (loader, filePath) => {
+    return new Promise((resolve, reject) => {
+        loader.load(
+            filePath,
+            (gltf) => resolve(gltf.scene),
+            (xhr) => console.log(`Model ${(xhr.loaded / xhr.total) * 100}% loaded`),
+            (error) => reject(error)
+        );
+    });
+};
+
+const geometry = new THREE.BoxGeometry();
+const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+const cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+camera.position.z = 5;
+function animate() {
+    requestAnimationFrame(animate);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+animate();
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
