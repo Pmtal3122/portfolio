8278ba6cd380a4d53d5b44080064e6dda683c50c
Pmtal3122 <talegaonkar.piyush@gmail.com>
2025-06-06 14:50:28 +0530

    3d scroll

--- utils/timeline.js
+++ utils/timeline.js
@@ -1,7 +1,11 @@
 const getTimeline = (camera) => {
     const tl = gsap.timeline();
+    const body = document.querySelector('body');
     const landingBlock = document.querySelector('#landing');
-    tl.to(camera.position, {
+    const workExpBlock = document.querySelector('#workEx');
+    const projectsBlock = document.querySelector('#projects');
+    const skillsBlock = document.querySelector('#skills');
+    tl.fromTo(camera.position, {x: 0, y: 0, z: -100}, {
         x: 0,
         y: 0,
         z: 2,
@@ -9,16 +13,60 @@ const getTimeline = (camera) => {
         ease: "power3.out"
     })
 
-    .fromTo(camera.position, {
-        x: 0, y: 0, z: 2
-    }, {
-        x: 0, y: -5, z: 2, scrollTrigger: {
-            trigger: landingBlock,
-            start: "top top",
-            end: "bottom top",
-            scrub: true,
+    .fromTo(camera.position, {x: 0, y: 0, z: 2}, {
+        x: 0, y: -40, z: 5, scrollTrigger: {
+            trigger: body,
+            start: "top 0%",
+            end: "bottom 0%",
+            scrub: true
         }
     })
+
+    // .fromTo(camera.position, {
+    //     x: 0, y: 0, z: 2
+    // }, {
+    //     x: 0, y: -10, z: 2, scrollTrigger: {
+    //         trigger: landingBlock,
+    //         start: "top 0%",
+    //         end: "bottom 0%",
+    //         scrub: true,
+    //         markers: true
+    //     }
+    // })
+
+    // .fromTo(camera.position, {
+    //     x: 0, y: -10, z: 2
+    // }, {
+    //     x: 0, y: -20, z: 2, scrollTrigger: {
+    //         trigger: workExpBlock,
+    //         start: "top 0%",
+    //         end: "bottom 0%",
+    //         scrub: true,
+    //     }
+    // })
+
+    // .fromTo(camera.position, {
+    //     x: 0, y: -20, z: 2
+    // }, {
+    //     x: 0, y: -30, z: 2, scrollTrigger: {
+    //         trigger: projectsBlock,
+    //         start: "top 0%",
+    //         end: "bottom 0%",
+    //         scrub: true,
+    //         // markers: true
+    //     }
+    // })
+
+    // .fromTo(camera.position, {
+    //     x: 0, y: -30, z: 2
+    // }, {
+    //     x: 0, y: -32, z: 2, scrollTrigger: {
+    //         trigger: skillsBlock,
+    //         start: "top 0%",
+    //         end: "bottom 0%",
+    //         scrub: true,
+    //     }
+    // });
 
     return tl;
 }
